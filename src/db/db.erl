b1d458f406f024a8822bf5baca250d5cb127ece8
Przemysław Dąbek <przemyslaw.dabek@gmail.com>
2011-08-11 14:00:25 +0200

    mochisup

---
+++ src/db/db.erl
@@ -0,0 +1,47 @@
+-module(db).
+-compile(export_all).
+-include_lib("stdlib/include/qlc.hrl").
+
+-record(device, {id, name, kernel, os}).
+-record(program, {id, name, version}).
+
+init() ->
+  mnesia:create_schema([node()]),
+  mnesia:start(),
+  mnesia:create_table(device, [{type, ordered_set}, {attributes, record_info(fields, device)}, {disc_copies, [node()]}]),
+  mnesia:create_table(program, [{type, ordered_set}, {attributes, record_info(fields, program)}, {disc_copies, [node()]}]),
+  mnesia:stop().
+
+start() ->
+  mnesia:start().
+
+stop() ->
+  mnesia:stop().
+
+create(Record) ->
+  mnesia:transaction(
+    fun() -> mnesia:write(Record) end
+  ).
+
+all(TableName) ->
+  Q = qlc:q([X || X <- mnesia:table(TableName)]),
+  F = fun() -> qlc:e(Q) end,
+  {atomic, Val} = mnesia:transaction(F),
+  Val.
+  
+find(TableName, Key) ->
+  F = fun() ->
+    mnesia:read(TableName, Key, write)
+  end,
+  {atomic, Val} = mnesia:transaction(F),
+  Val.
+  
+destroy(TableName, Key) ->
+  F = fun() ->
+    mnesia:delete(TableName, Key, write)
+  end,
+  mnesia:transaction(F).
+  
+destroy_all(TableName) ->
+  mnesia:clear_table(TableName).
+
---
+++ src/mochisup_web.erl
@@ -0,0 +1,62 @@
+%% @author Mochi Media <dev@mochimedia.com>
+%% @copyright 2010 Mochi Media <dev@mochimedia.com>
+
+%% @doc Web server for mochisup.
+
+-module(mochisup_web).
+-author("Mochi Media <dev@mochimedia.com>").
+
+-export([start/1, stop/0, loop/2]).
+
+%% External API
+
+start(Options) ->
+    {DocRoot, Options1} = get_option(docroot, Options),
+    Loop = fun (Req) ->
+                   ?MODULE:loop(Req, DocRoot)
+           end,
+    mochiweb_http:start([{name, ?MODULE}, {loop, Loop} | Options1]).
+
+stop() ->
+    mochiweb_http:stop(?MODULE).
+
+loop(Req, DocRoot) ->
+    "/" ++ Path = Req:get(path),
+    try
+      [Controller | Args] = lists:map(fun(X) -> binary_to_list(X) end, re:split(Path, "/")),
+      case Controller of
+        "devices" ->
+          devices_controller:dispatch(Req, Args);
+        _ ->
+          Req:serve_file(Path, DocRoot)
+      end
+    catch
+        Type:What ->
+            Report = ["web request failed",
+                      {path, Path},
+                      {type, Type}, {what, What},
+                      {trace, erlang:get_stacktrace()}],
+            error_logger:error_report(Report),
+            %% NOTE: mustache templates need \ because they are not awesome.
+            Req:respond({500, [{"Content-Type", "text/plain"}],
+                         "request failed, sorry\n"})
+    end.
+
+%% Internal API
+
+get_option(Option, Options) ->
+    {proplists:get_value(Option, Options), proplists:delete(Option, Options)}.
+
+%%
+%% Tests
+%%
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+
+you_should_write_a_test() ->
+    ?assertEqual(
+       "No, but I will!",
+       "Have you written any tests?"),
+    ok.
+
+-endif.

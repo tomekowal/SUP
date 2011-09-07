62474e8ab0f3ca62e55ff6d8a7c222dba5212eb4
Przemysław Dąbek <przemyslaw.dabek@gmail.com>
2011-09-07 20:35:18 +0200

    clean branch

--- server/src/controller/main_controller.erl
+++
@@ -1,6 +0,0 @@
--module(main_controller).
--compile(export_all).
-
-index(Req) ->
-  {ok, HTMLOutput} = main_index_dtl:render([]),
-  Req:respond({200, [{"Content-Type", "text/html"}], HTMLOutput}).
--- server/src/db/sup_db.hrl
+++
@@ -1,4 +0,0 @@
-%% Record definitions
-
--record(device, {id, name, kernel, os, ip, port, message, programs}).
--record(program, {id, name, version, description}).

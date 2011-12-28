409f40387987b0904abab809d25a9b5c0528aa88
ghik <romeqjanoosh@gmail.com>
2011-12-28 18:23:54 +0100

    some docs

--- beagle/apps/sup_beagle/src/sup_beagle_maintenance.erl
+++ beagle/apps/sup_beagle/src/sup_beagle_maintenance.erl
@@ -3,6 +3,7 @@
 
 %% this module is used by debian package maintainer scripts through nodetool rpc or directly as erl -s param
 
+% Creates RELEASES file in 'releases' directory based on a .rel file.
 create_RELEASES([TargetRoot, Release, Version, halt]) ->
     application:load(sasl),
     create_RELEASES(lists:map(fun atom_to_list/1, [TargetRoot, Release, Version])),
@@ -16,6 +17,8 @@ create_RELEASES([TargetRoot, Release, Version]) ->
       ),
     ok.
 
+% Performs manual upgrade of the node. This includes manual generation of RELEASES file
+% and manual removal of any applications that are not a part of new version of release.
 manual_upgrade([TargetRoot, Release, Version, OldVersion, halt]) ->
     application:load(sasl),
     manual_upgrade(lists:map(fun atom_to_list/1, [TargetRoot, Release, Version, OldVersion])),
@@ -34,11 +37,13 @@ manual_upgrade([TargetRoot, Release, Version, OldVersion]) ->
     ok = remove_recursively([RelDir]),
     ok.
 
+% Fetches a list of application names and versions from .rel file.
 applications_from_rel(RelFile) ->
     {ok, [{release, {_Release, _Version}, {erts, _ErtsVsn}, NewAppTuples}]} =
         file:consult(RelFile),
     lists:map(fun({App,Vsn}) -> atom_to_list(App)++"-"++Vsn end, NewAppTuples).
 
+% Removes a directory from the filesystem, recursively with all its contents.
 remove_recursively([Path | Rest]) ->
     case filelib:is_dir(Path) of
         true ->
@@ -55,6 +60,7 @@ remove_recursively([Path | Rest]) ->
 remove_recursively([]) ->
     ok.
 
+% Performs hot-upgrade of the node.
 upgrade_release([TargetRoot, Release, Version]) ->
     RelFile = filename:join([TargetRoot, "releases", Version, Release++".rel"]),
     {ok, Version} = release_handler:set_unpacked(RelFile, []),
